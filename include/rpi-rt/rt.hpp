b724046403706ac676187884c2a56154823ab6f8
vedran97 <vedantmranade@gmail.com>
2023-03-22 05:13:21 +0000

    Added function calls needed to setup a thread

--- include/rpi-rt/rt.hpp
+++ include/rpi-rt/rt.hpp
@@ -1,18 +1,42 @@
 #ifndef RPI_RT_H_
 #define RPI_RT_H_
 namespace rpi_rt{
+enum class CPUS{
+CPU1,
+CPU2,
+CPU3,
+CPU4
+};
 class rt_settings
 {
 private:
     /* data */
+    const CPUS affCPU;
+    const int priority;
+    const int memoryToBeLocked;
 public:
-    rt_settings(/* args */);
+    rt_settings(CPUS affCPUs,int priority,int memoryToBeLocked)
+    :affCPU(affCPUs),
+    priority(priority),
+    memoryToBeLocked(memoryToBeLocked)
+    {
+
+    }
+    /// @brief Pins the calling thread to a CPU Core mentioned in the constructor
+    void applyAffinity();
+
+    /// @brief Applies specified thread priority to the calling thread
+    void applyPriority();
+
+    /// @brief Allocates and locks memory for the caller
+    void applyMemoryLock();
+
+    /// @brief Changes power governance mode of CPU to high performance mode
+    void applyHighPerformancemode();
+
+    /// @brief Changes RT_SCHED time to -1
+    void applySchedulingTimeChange();
 };
-
-rt_settings::rt_settings(/* args */)
-{
-
-}
 
 };
 #endif // RPI_RT_H_
--- src/rpi-rt/rt.cpp
+++ src/rpi-rt/rt.cpp
@@ -2,5 +2,25 @@
 
 namespace rpi_rt{
 
+void rt_settings::applyAffinity(){
+
+}
+
+
+void rt_settings::applyPriority(){
+
+}
+
+void rt_settings::applyMemoryLock(){
+
+}
+
+void rt_settings::applyHighPerformancemode(){
+
+}
+
+void rt_settings::applySchedulingTimeChange(){
+
+}
 
 }
